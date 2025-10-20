271a213ccaf3153536c567ae82ad57c29e285154
Sicco <sdotter@gmail.com>
2025-10-20 22:47:11 +0200

    fix: typo

--- reset.bat
+++ reset.bat
@@ -1,10 +1,10 @@
 @echo off
 
-REM This script will stop and remove all Docker containers related to your LAMP stack
+REM This script will stop and remove all Docker containers related to your LEMP stack
 REM Run as administrator if needed
 
 REM Make sure the reset script is executable
-wsl chmod +x "/mnt/c/[ DEV ]/[ LAMP ]/reset_lemp.sh"
+wsl chmod +x "/mnt/c/[ DEV ]/[ LEMP ]/reset_lemp.sh"
 
 REM Run the reset script in WSL
-wsl bash "/mnt/c/[ DEV ]/[ LAMP ]/reset_lemp.sh"
+wsl bash "/mnt/c/[ DEV ]/[ LEMP ]/reset_lemp.sh"
